c875316184a480e4cc04ef9e68ba0b104b5e85e3
Andrew Pennebaker <apennebaker@42six.com>
2014-02-18 14:16:44 -0500

    curl idioms

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     exec: {
       backgroundServer: "node fizzbuzz.js &",
 			waitForServer: "sleep 2",
-			loadPage: "curl http://localhost:8124 2>/dev/null",
+			loadPage: "curl -s http://localhost:8124",
 			killServer: "lsof -i tcp:8124 | awk 'NR!=1 {print $2}' | xargs kill"
     }
   });
