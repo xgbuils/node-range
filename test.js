4625aa78257c0dc16f7c647d0f12283a8650a647
Fábio Santos <fabio-s-santos@telecom.pt>
2014-05-08 14:14:16 +0100

    support negative step

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ var cmp = require('cmp');
 assert(cmp.eq(range(0, 10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 'Simple range');
 assert(cmp.eq(range(0, 10, 2), [0, 2, 4, 6, 8]), 'Range with step');
 assert(cmp.eq(range.range(0, 10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 'range property test');
+assert(cmp.eq(range.range(10, 0, -1), [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), 'negative step');
+
