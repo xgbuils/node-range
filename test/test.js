a836f6719e3f78236949388ac5f0f70336c9d8d9
Fábio Santos <fabiosantosart@gmail.com>
2015-05-16 16:33:10 +0100

    range(n) should be equivalent to range(0, n)

--- test/test.js
+++ test/test.js
@@ -13,6 +13,8 @@ describe("range", function() {
       assert(cmp.eq(range.range(0, 10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), "range property test");
       assert(cmp.eq(range.range(10, 0, -1), [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), "negative step");
 
+      assert(cmp.eq(range.range(5), [0, 1, 2, 3, 4]), 'When called with one argument, that argument is assumed to be the end of the range, and the beginning will be zero.');
+
       assert(cmp.eq(range.range(0, 3, 2), [0, 2]), "regression: if step is not divisible by abs(start - stop) we get an infinite loop");
 
       assert(cmp.eq(range.range(3, 0, -2), [3, 1]), "regression: if step is not divisible by abs(start - stop) we get an infinite loop (negative step now)");
